9294e5d2e9d938c92b2636aaad7aaa633c96bdc4
ffreylaw <ffreylaw@gmail.com>
2017-05-08 14:01:41 +1000

    update

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <inttypes.h>
+#include <stdbool.h>
 
 #include "uint256.h"
 #include "sha256.h"
@@ -38,17 +38,14 @@ int main(int argc, char const *argv[]) {
     printf("0x%llx\n", solution);
 
     BYTE seed[32];
+    uint256_init(seed);
     char buf2[2];
     for (i = 0; i < 64; i+=2) {
         buf2[0] = array[2][i];
         buf2[1] = array[2][i+1];
         seed[i/2] = strtoull(buf2, NULL, 16);
     }
-
-    printf("0x");
-    for (i = 0; i < 32; i++) {
-        printf("%02x", seed[i]);
-    }
+    print_uint256(seed);
 
     printf("\n%d\n", 0b1 << 10);
 
@@ -92,14 +89,45 @@ int main(int argc, char const *argv[]) {
     print_uint256(target);
 
 
+    BYTE soln[32];
+    uint256_init(soln);
 
-    BYTE buf4[SHA256_BLOCK_SIZE];
+    char *text = (char*)malloc(sizeof(char));
+    int count = 0;
+    //bool flag = false;
+    char *buf4 = (char*)malloc(2*sizeof(char));
+    for (i = 0; i < 32; i++) {
+        // if (!flag && (seed[i] != 0x0)) {
+        //     flag = true;
+        // }
+        //if (flag) {
+            count+=2;
+            text = (char*)realloc(text, count * sizeof(char));
+            sprintf(buf4, "%02x", seed[i]);
+            text[count-2] = buf4[0];
+            text[count-1] = buf4[1];
+        //}
+    }
+    char *buf5 = (char*)malloc((32 + 1) * sizeof(char));
+    sprintf(buf5, "%llx", solution);
+    for (i = 0; i < strlen(buf5); i++) {
+        count++;
+        text = (char*)realloc(text, count * sizeof(char));
+        text[count-1] = buf5[i];
+    }
+    printf("text: %s\n", text);
+
+    BYTE buf6[SHA256_BLOCK_SIZE];
 	SHA256_CTX ctx;
-	int idx;
 
-	// sha256_init(&ctx);
-	// sha256_update(&ctx, text1, strlen(text1));
-	// sha256_final(&ctx, buf4);
+	sha256_init(&ctx);
+	sha256_update(&ctx, text, strlen(text));
+	sha256_final(&ctx, buf6);
+
+    print_uint256(buf6);
+
+    BYTE buf7[SHA256_BLOCK_SIZE];
+    
 
     return 0;
 }
